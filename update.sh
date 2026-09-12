32d55a03b9cc695be5be135e99b78eff4c93f77e
Karoeba <m.tera.bv0x0vd+git@gmail.com>
2026-09-12 23:14:36 +0900

    Set v1.1.1 startup version and stable 1.1.x deployment names

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+VERSION="1.1.1"
 CONFIG="/app/mydns.conf"
 STATE_DIR="/state"
 STATE_FILE="$STATE_DIR/state.conf"
@@ -248,7 +249,7 @@ while true; do
     CHECK_INTERVAL=300
     if load_config; then
         if [ "$STARTUP_LOGGED" -eq 0 ]; then
-            log "[STARTUP] MyDNS updater started: CHECK_INTERVAL=${CHECK_INTERVAL}s, FORCE_UPDATE_INTERVAL=${FORCE_UPDATE_INTERVAL}s"
+            log "[STARTUP] MyDNS updater v${VERSION} started: CHECK_INTERVAL=${CHECK_INTERVAL}s, FORCE_UPDATE_INTERVAL=${FORCE_UPDATE_INTERVAL}s"
             STARTUP_LOGGED=1
         fi
         run_cycle
